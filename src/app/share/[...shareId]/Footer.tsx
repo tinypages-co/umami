46461727a421618ff8dfa0f422f695815a2f8ee0
jeromevvb <7522096+jeromevvb@users.noreply.github.com>
2025-06-21 15:28:03 +0700

    tiny notes

--- src/app/share/[...shareId]/Footer.tsx
+++ src/app/share/[...shareId]/Footer.tsx
@@ -4,9 +4,7 @@ import styles from './Footer.module.css';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href={HOMEPAGE_URL}>
-        <b>umami</b> {`v${CURRENT_VERSION}`}
-      </a>
+        <b>TinyStats</b> {`v${CURRENT_VERSION}`}
     </footer>
   );
 }

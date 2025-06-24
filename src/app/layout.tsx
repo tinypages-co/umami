1dc5e78ce1cca8ee046195c446a6d6f285eaa79c
jeromevvb <7522096+jeromevvb@users.noreply.github.com>
2025-06-24 16:44:04 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function ({ children }) {
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Umami',
-    default: 'Umami',
+    template: '%s | TinyStats',
+    default: 'TinyStats',
   },
 };
